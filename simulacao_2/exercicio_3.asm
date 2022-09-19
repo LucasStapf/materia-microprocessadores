3c8efe6527f593f3663dcc2fe497e369226e5911
LucasStapf <lucas.c.stapf@gmail.com>
2022-09-19 16:10:48 -0300

    Finalizado terceiro exercício

--- simulacao_2/exercicio_3.asm
+++ simulacao_2/exercicio_3.asm
@@ -3,30 +3,93 @@
 ; NUSP 11800559
 ; ******************************* ;
 
-; ************************************* ;
-; P1.7(0) esteira posicionada na esquerda
-; P1.0(0) esteira posicionada na direita
-; ************************************* ;
+; ************************************** ;
+; A esteira é simulada pelos LEDs. O LED
+; que estiver ligado indica a posição do
+; pacote. LEDs estão ligados na porta P1.
+; ************************************** ;
 
 	ORG	0
 	SJMP	PROG
 
-	ORG	0003h
+	ORG	3h
+	ACALL	INV_MOV
+	RETI
+
+	ORG	13h
+	ACALL	INV_MOV
 	RETI
 
 PROG:
 	SETB	EA
 	SETB	IE.0
-	SETB	IE0
-	CLR	P1.0 	
+	SETB	IT0
+	SETB	IE.2
+	SETB	IT1
 
-INV_ROT:
-	CPL	P2.1
-	CPL	P2.7
+	SETB	P2.7
+	CLR	P2.1
+	
+	CLR	P1.0
+	MOV 	A,P1
+
+ESQ:	JB	P2.1, DIR
+	ACALL	MOV_ESQ
+	SJMP	ESQ
+DIR:	JB	P2.7, ESQ
+	ACALL	MOV_DIR
+	SJMP	DIR
+	
+MOV_ESQ:
+	RL	A
+	MOV 	P1, A
+	ACALL 	DLY_LED
+	RET
+	
+MOV_DIR:
+	RR	A
+	MOV	P1, A
+	ACALL	DLY_LED
 	RET
 
-DLY_1s:
+DLY_LED:
+	CLR 	EA
+	MOV	R0, #0E4h
+	NOP
+	DJNZ	R0, $
+	NOP
+	NOP
+	NOP
+	SETB 	EA
+	RET
 
+INV_MOV:
+	CPL	P2.1
+	CPL	P2.7
+
+	CLR	EA
+	JB	P1.7, DLY_10S 
 DLY_5s:
-
+	MOV	R1, #02Fh
+	MOV	R0, #02Fh
+	NOP
+	DJNZ	R0, $
+	DJNZ	R1, $-5
+	NOP
+	SJMP	END_INV
+DLY_10s:
+	MOV	R2, #002h
+	MOV	R1, #087h
+	MOV	R0, #00Fh
+	NOP
+	DJNZ	R0, $
+	DJNZ	R1, $-5
+	DJNZ	R2, $-9
+	MOV	R0, #00Dh
+	DJNZ	R0, $
+	NOP
+END_INV:
+	SETB	EA
+	RET
+	
 	END
