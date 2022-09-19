860a9023d84fc605ecec1f2c887caa57da8d0059
LucasStapf <lucas.c.stapf@gmail.com>
2022-09-19 08:41:01 -0300

    Adicionado código do segundo exercício

---
+++ simulacao_2/exercicio_2.asm
@@ -0,0 +1,32 @@
+; ******************************* ;
+; Lucas Carvalho Freiberger Stapf
+; NUSP 11800559
+; ******************************* ;
+
+	ORG	0
+LED1:	JB	P3.5,LED2
+	CPL	P1.0
+LED2:	JB	P3.6,LED3
+	CPL	P1.1
+LED3:	JB	P3.7,DLY_1HZ
+	CPL	P1.2
+	CPL	P1.0
+DLY_1Hz:
+	MOV	R2, #00Eh
+	MOV	R1, #089h
+	MOV	R0, #0EEh
+	NOP
+	DJNZ	R0, $
+	DJNZ	R1, $-5
+	DJNZ	R2, $-9
+	MOV	R1, #002h
+	MOV	R0, #0DEh
+	NOP
+	DJNZ	R0, $
+	DJNZ	R1, $-5
+	NOP
+	NOP
+	NOP
+	NOP
+	JMP	LED1
+	END
